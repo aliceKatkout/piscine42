1d55d771feeb9490d84fe8cd7e7077d91e4fe5b3
Alice Vedrenne <avedrenn@e3r2p2.clusters.42paris.fr>
2022-08-19 13:08:48 +0200

    cou

--- c_07/ex01/ft_range.c
+++ c_07/ex01/ft_range.c
@@ -1,49 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: avedrenn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/19 11:14:41 by avedrenn          #+#    #+#             */
-/*   Updated: 2022/08/19 12:21:26 by avedrenn         ###   ########.fr       */
+/*   Created: 2022/08/19 12:18:27 by avedrenn          #+#    #+#             */
+/*   Updated: 2022/08/19 13:02:54 by avedrenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
+
 #include <stdlib.h>
+#include <stdio.h>
 
-int	ft_len(char *src)
+int	*ft_range(int min, int max)
 {
+	int	*res;
+	int	size;
 	int	i;
+	int	j;
 
-	i = 0;
-	while (src[i] != 0)
-		i++;
-	return (i);
-}
-
-char	*ft_strdup(char *src)
-{
-	char	*res;
-	int		i;
-
-	i = 0;
-	res = (char*)malloc(sizeof(char) * (ft_len(src) + 1));
-	while (src[i] != 0)
+	if (min >= max)
+		return (0);
+	size = max - min;
+	res = (int*)malloc(sizeof(int) * size);
+	i = min;
+	j = 0;
+	while (i < max)
 	{
-		res[i] = src[i];
+		res[j] = i;
 		i++;
+		j++;
 	}
-	res[i] = '\0';
 	return (res);
 }
 
-//int	main()
-//{
-//	char src[] = "Coucou :)";
-//	char *res;
-//
-//	res = ft_strdup(src);
-//	free(res);
-//	return (0);
-//}
+int	main(void)
+{
+	int	*res;
+	int	i;
+	res = ft_range(-2, 20);
+	for (i = 0; i < 22; i++)
+		printf("%d\n", res[i]);
+	free(res);
+	return (0);
+}
